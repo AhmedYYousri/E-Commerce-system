dec28d7951a4b850475b4c529b03c7299a8b02f7
AhmedYYousri <Ahmedyousri2003@gmail.com>
2023-03-01 22:50:55 +0200

    push

---
+++ OnlineShopping/OnlineShopping/Models/CategoryDetail.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShopping.Models
+{
+    public class CategoryDetail
+    {
+        public int CategoryId { get; set; }
+        [Required (ErrorMessage ="Category Name Requird")]
+        [StringLength(100, ErrorMessage = "Minimum 3 and Minimum 5 and Maximum 100 charaters are allwed", MinimumLength = 3 )]
+        public string CategoryName { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<bool> IsDelete { get; set; }
+
+    }
+
+    public class ProductDetail
+    {
+        public int ProductId { get; set; }
+        [Required(ErrorMessage = "Category Name Requird")]
+        [StringLength(100, ErrorMessage = "Minimum 3 and Minimum 5 and Maximum 100 charaters are allwed", MinimumLength = 3)]
+        public string ProductName { get; set; }
+        [Required]
+        [Range(1,50)]
+        public Nullable<int> CategoryId { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<bool> IsDelete { get; set; }
+        public Nullable<System.DateTime> CategoryDate { get; set; }
+        public Nullable<System.DateTime> ModifieDate { get; set; }
+        public Nullable<System.DateTime> Description { get; set; }
+        [Required(ErrorMessage = "Description is Required")]
+        public string ProductImage { get; set; }
+        public Nullable<bool> IsFeatured { get; set; }
+        [Required]
+        [Range(typeof(int) , "1","500" ,ErrorMessage = "Invalid Quantity")]
+        public Nullable<int> Quantitya { get; set; }
+        [Required]
+        [Range(typeof(int), "1", "200000000", ErrorMessage = "Invalid Price")]
+        public Nullable<decimal> Price { get; set; }
+
+        public SelectList Categories { get; set; }
+            
+    }
+
+
+}
---
+++ OnlineShopping/OnlineShopping/Models/Home/HomeIndexViewModel.cs
@@ -0,0 +1,36 @@
+﻿using OnlineShopping.DAL;
+using OnlineShopping.Repository;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+using iTextSharp.text;
+
+namespace OnlineShopping.Models.Home
+{
+    public class HomeIndexViewModel
+    {
+        public GenericUnitOfWork _uniteOfWork = new GenericUnitOfWork();
+        readonly dbMyOnilineShoppingEntities1 context = new dbMyOnilineShoppingEntities1();
+        public IPagedList<Tbl_Product> ListOfProducts { get; set; }
+        public HomeIndexViewModel CreateModel(string search, int pageSize, int? page)
+        {
+            SqlParameter[] param = new SqlParameter[]{
+                new SqlParameter("@search",search??(object)DBNull.Value)
+            };
+            IPagedList<Tbl_Product> data = context.Database.SqlQuery<Tbl_Product>("GetBySearch @search", param).ToList().ToPagedList(page ?? 1, pageSize);
+            return new HomeIndexViewModel
+            {
+                ListOfProducts = data
+            };
+        }
+
+    }
+}
